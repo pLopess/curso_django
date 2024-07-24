67fdb50ee6fbd1bafd91105bec6c267eefcf1432
Pedro Lopes <87770252+pLopess@users.noreply.github.com>
2024-07-24 18:13:37 -0300

    starting project

---
+++ curso_django/myproject/myproject/views.py
@@ -0,0 +1,11 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello World! Here is home :)")
+    return render(request, 'home.html')
+
+
+def about(request):
+    # return HttpResponse("My About page")
+    return render(request, 'about.html')
